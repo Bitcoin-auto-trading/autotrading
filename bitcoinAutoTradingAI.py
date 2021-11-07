05aaefba582c3a00bb67b8a1a213b301c0af989e
ggoniggoni <51435556+good9804@users.noreply.github.com>
2021-11-08 00:33:41 +0900

    Update bitcoinAutoTradingAI.py

--- bitcoinAutoTradingAI.py
+++ bitcoinAutoTradingAI.py
@@ -68,7 +68,7 @@ while True:
         schedule.run_pending()
 
         if start_time < now < end_time - datetime.timedelta(seconds=10):
-            target_price = get_target_price("KRW-BTC", 0.5)
+            target_price = get_target_price("KRW-BTC", 0.4)
             current_price = get_current_price("KRW-BTC")
             if target_price < current_price and current_price < predicted_close_price:
                 krw = get_balance("KRW")
