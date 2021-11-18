8cf4da1c225f78be0b4bd5f46dcea5ba0489a8f3
ggoniggoni <51435556+good9804@users.noreply.github.com>
2021-11-18 21:16:32 +0900

    Create realkakao.py

---
+++ realkakao.py
@@ -0,0 +1,26 @@
+import json
+import requests
+
+with open(r"C:\Users\동생아 좋냐\source\repos\kakao\kakao\kakao_code.json","r") as fp:
+    tokens = json.load(fp)
+
+url="https://kapi.kakao.com/v2/api/talk/memo/default/send"
+
+# kapi.kakao.com/v2/api/talk/memo/default/send 
+
+headers={
+    "Authorization" : "Bearer " + tokens["access_token"]
+}
+
+data={
+    "template_object": json.dumps({
+        "object_type":"text",
+        "text":"Hello, world!",
+        "link":{
+            "web_url":"www.naver.com"
+        }
+    })
+}
+
+response = requests.post(url, headers=headers, data=data)
+response.status_code
