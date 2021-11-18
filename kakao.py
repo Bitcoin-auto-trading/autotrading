86acbd0612baf54ea01fd60157813c1b3a63cbe5
good9804 <good9804@naver.com>
2021-11-18 17:28:18 +0900

    2차 카카오

--- kakao.py
+++ kakao.py
@@ -38,3 +38,22 @@ class Kakao:
         }
 
         requests.post(self.url, headers=self.headers, data=data)
+if __name__ == "__main__":
+    import pyupbit
+
+    access_key = "Uc7gjRjwxKWtqi3CzE8eBa0GBxKEuvxstqy4VBux"
+    secret_key = "BuXKRKaxcPdhL8Htpl1cGsVbqGh0zd10DHVLLxWB"
+    upbit = pyupbit.Upbit(access_key, secret_key)
+
+    kakao = Kakao()
+    ticker = 'krw-btc'
+
+    orderbook = pyupbit.get_orderbook(ticker)
+    bids_asks = orderbook[0]['orderbook_units']
+    bid_price = bids_asks[0]['bid_price']
+
+    sellprice = bid_price
+    balance = round(float(upbit.get_balances()[0]['balance']), 2)
+    ror = 0.98999948
+    message = f'{ticker}를 {sellprice}에 \n전량매도 했습니다.\n\n매도 체결 후 잔액:\n{balance}원\n\n현재 수익률: {round((ror-1)*100, 2)}%'
+    kakao.send_message2me(message)
