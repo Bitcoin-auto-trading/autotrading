ef59a4ea7c770ffdd24e659553a4a24d95407ebc
ggoniggoni <51435556+good9804@users.noreply.github.com>
2021-11-21 01:40:28 +0900

    Update kakao.py

--- kakao.py
+++ kakao.py
@@ -1,59 +1,55 @@
+import schedule
+import requests
+import json
+
+url = "https://kauth.kakao.com/oauth/token"
+
+data = {
+    "grant_type" : "authorization_code",
+    "client_id" : "9fe6eedbb07dcabc2528454a986e5a8e",
+    "redirect_url" : "https://localhost:3000",
+    "code" : "hpQ6hu5tQ9vIFKaJ4QdJpwcRarGM7eFOaYMGJazUjnrZ2ya1xSDioJlBgdkdOK6HgtRxlAo9dVoAAAF9Phbmdg"
+}
 import json
 import requests
 
+url = "https://kapi.kakao.com/v2/api/talk/memo/default/send"
 
-class Kakao:
-    def __init__(self):
-        with open("kakao_code.json", "r") as fp:
-            tokens = json.load(fp)
-
-        auth_url = "https://kauth.kakao.com/oauth/token"
-        data = {
-            "grant_type": "refresh_token",
-            "client_id": "6bea89b579e0495940a0b8989903b22f",
-            "refresh_token": tokens["refresh_token"]
-        }
-        response = requests.post(auth_url, data=data)
-        tokens = response.json()
-
-        url = "https://kapi.kakao.com/v2/api/talk/memo/default/send"
-
-        headers = {
-            "Authorization": "Bearer " + tokens['access_token']
-        }
-
-        self.url = url
-        self.headers = headers
+# 사용자 토큰
+headers = {
+    "Authorization": "Bearer _8VIS8eQDVTImFIspnOW2n8T7ue2qKlOJcv3TQo9dZwAAAF9PiGfRA"
+}
 
 
-    def send_message2me(self, message):
-        data = {
-            "template_object": json.dumps({
-                "object_type": "text",
-                "text": message,
-                "link": {
-                    "web_url": "www.naver.com"
-                }
-            })
-        }
+data = {
+    "template_object" : json.dumps({ "object_type" : "text",
+                                     "text" : "Lil bapy",
+                                     "link" : {
+                                                 "web_url" : "www.naver.com"
+                                              }
+    })
+}
 
-        requests.post(self.url, headers=self.headers, data=data)
-if __name__ == "__main__":
-    import pyupbit
+response = requests.post(url, headers=headers, data=data)
+print(response.status_code)
+if response.json().get('result_code') == 0:
+    print('메시지를 성공적으로 보냈습니다.')
+else:
+    print('메시지를 성공적으로 보내지 못했습니다. 오류메시지 : ' + str(response.json()))
 
-    access_key = "Uc7gjRjwxKWtqi3CzE8eBa0GBxKEuvxstqy4VBux"
-    secret_key = "BuXKRKaxcPdhL8Htpl1cGsVbqGh0zd10DHVLLxWB"
-    upbit = pyupbit.Upbit(access_key, secret_key)
+schedule.every(3).hours.do(job)
+    
+   
+def job():
+    url = "https://kauth.kakao.com/oauth/token"
+    data = {
+    "grant_type": "refresh_token",
+    "client_id": "9fe6eedbb07dcabc2528454a986e5a8e",
+    "refresh_token": "_eR7dCU5CFUylxS65DWE1fbEs9O9tx2K7NvpRwo9dZwAAAF9PiGfQw"
+}
+    response = requests.post(url, data=data)
+    tokens = response.json()
 
-    kakao = Kakao()
-    ticker = 'krw-btc'
-
-    orderbook = pyupbit.get_orderbook(ticker)
-    bids_asks = orderbook[0]['orderbook_units']
-    bid_price = bids_asks[0]['bid_price']
-
-    sellprice = bid_price
-    balance = round(float(upbit.get_balances()[0]['balance']), 2)
-    ror = 0.98999948
-    message = f'{ticker}를 {sellprice}에 \n전량매도 했습니다.\n\n매도 체결 후 잔액:\n{balance}원\n\n현재 수익률: {round((ror-1)*100, 2)}%'
-    kakao.send_message2me(message)
+# kakao_code.json 파일 저장
+    with open("kakao_code.json", "w") as fp:
+        json.dump(tokens, fp)#액세스토큰갱신
