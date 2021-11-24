0c3add3dc84cc9daa618e5eb43eacfb5538c17c0
song <sod723@naver.com>
2021-11-24 20:09:08 +0900

    Update main.py

--- main.py
+++ main.py
@@ -7,6 +7,43 @@ from pyupbit import Upbit
 import datetime
 from PyQt5.QtCore import QThread, pyqtSignal
 from volatility2 import *
+import requests
+import json
+import os.path
+
+class Kakao:
+    def __init__(self):
+        with open("kakao_code.json","r") as fp:
+             tokens = json.load(fp)
+
+             auth_url = "https://kauth.kakao.com/oauth/token"
+        data = {
+            "grant_type": "refresh_token",
+            "client_id": "9fe6eedbb07dcabc2528454a986e5a8e",
+            "refresh_token" : tokens["refresh_token"]
+        }
+        response = requests.post(auth_url, data=data)
+        tokens = response.json()
+
+        url = "https://kapi.kakao.com/v2/api/talk/memo/default/send"
+
+        headers = {
+            "Authorization": "Bearer " + tokens['access_token']
+        }
+
+        self.url = url
+        self.headers = headers
+    def send_message2me(self, message):
+        data = {
+            "template_object": json.dumps({
+                "object_type": "text",
+                "text": message,
+                "link": {
+                    "web_url": "www.naver.com"
+                }
+            })
+        }
+        requests.post(self.url, headers=self.headers, data=data)
 
 class VolatilityWorker(QThread):
     tradingSent = pyqtSignal(str, str, str)
@@ -69,7 +106,7 @@ class VolatilityWorker2(QThread):
     def run(self):
         now = datetime.datetime.now()
         mid = datetime.datetime(now.year, now.month, now.day) + datetime.timedelta(1)
-        ma5 = get_yesterday_ma5(self.ticker)
+        ma15 = get_ma15(self.ticker)
         target_price = get_target_price(self.ticker)
         wait_flag = False
         print("target price :", target_price)
@@ -79,23 +116,23 @@ class VolatilityWorker2(QThread):
                 if mid < now < mid + datetime.delta(seconds=10):
                     target_price = get_target_price(self.ticker)
                     mid = datetime.datetime(now.year, now.month, now.day) + datetime.timedelta(1)
-                    ma5 = get_yesterday_ma5(self.ticker)
+                    ma15 = get_ma15(self.ticker)
                     desc = sell_crypto_currency(self.upbit, self.ticker)
 
                     result = self.upbit.get_order(desc['uuid'])
                     timestamp = result['created_at']
-                    dt = datetime.datetime.fromtimestamp( int(int(timestamp)/1000000) )
+                    dt = datetime.datetime.fromtimestamp(int(int(timestamp) / 1000000))
                     tstring = dt.strftime("%Y/%m/%d %H:%M:%S")
                     self.tradingSent.emit(tstring, "매도", result['data']['order_qty'])
                     wait_flag = False
 
                 if wait_flag == False:
                     current_price = pyupbit.get_current_price(self.ticker)
-                    if (current_price > target_price) and (current_price > ma5):
+                    if (current_price > target_price) and (current_price > ma15):
                         desc = buy_crypto_currency(self.upbit, self.ticker)
                         result = self.upbit.get_order(desc['uuid'])
                         timestamp = result['created_at']
-                        dt = datetime.datetime.fromtimestamp( int(int(timestamp)/1000000) )
+                        dt = datetime.datetime.fromtimestamp(int(int(timestamp) / 1000000))
                         tstring = dt.strftime("%Y/%m/%d %H:%M:%S")
                         self.tradingSent.emit(timestamp, "매수", result['volume'])
                         wait_flag = True
@@ -118,7 +155,7 @@ class VolatilityWorker3(QThread):
     def run(self):
         now = datetime.datetime.now()
         mid = datetime.datetime(now.year, now.month, now.day) + datetime.timedelta(1)
-        ma5 = get_yesterday_ma5(self.ticker)
+        ma30 = get_ma30(self.ticker)
         target_price = get_target_price(self.ticker)
         wait_flag = False
         print("target price :", target_price)
@@ -128,7 +165,7 @@ class VolatilityWorker3(QThread):
                 if mid < now < mid + datetime.delta(seconds=10):
                     target_price = get_target_price(self.ticker)
                     mid = datetime.datetime(now.year, now.month, now.day) + datetime.timedelta(1)
-                    ma5 = get_yesterday_ma5(self.ticker)
+                    ma30 = get_ma30(self.ticker)
                     desc = sell_crypto_currency(self.upbit, self.ticker)
 
                     result = self.upbit.get_order(desc['uuid'])
@@ -140,7 +177,7 @@ class VolatilityWorker3(QThread):
 
                 if wait_flag == False:
                     current_price = pyupbit.get_current_price(self.ticker)
-                    if (current_price > target_price) and (current_price > ma5):
+                    if (current_price > target_price) and (current_price > ma30):
                         desc = buy_crypto_currency(self.upbit, self.ticker)
                         result = self.upbit.get_order(desc['uuid'])
                         timestamp = result['created_at']
@@ -170,19 +207,12 @@ class MainWindow(QMainWindow, form_class):
 
         self.setWindowTitle("Home Trading System")
 
-        with open("api.txt") as f:
+        with open("upbit.txt") as f:
             lines = f.readlines()
             apikey = lines[0].strip()
             seckey = lines[1].strip()
             self.apiKey.setText(apikey)
             self.secKey.setText(seckey)
-
-    def clickBtn4(self):
-
-        tickerKey = self.tickerKey.text()
-        change_ticker(tickerKey)
-        self.ticker = change_ticker(tickerKey)
-        QMessageBox.information(self, "코인정보", "코인이 {}로 바뀌었습니다.".format(self.ticker))
 
     def clickBtn(self):
         if self.button.text() == "매매시작":
@@ -197,7 +227,7 @@ class MainWindow(QMainWindow, form_class):
                 if self.balance == None:
                     self.textEdit.append("KEY가 올바르지 않습니다.")
                     return
-
+            QMessageBox.information(self, "매매정보", "자동매매1번입니다.")
             self.button.setText("매매중지")
             self.textEdit.append("------ START ------")
             self.textEdit.append("코인 종목 : {}   매매시작 가격 : {}".format(self.ticker,get_target_price(self.ticker)))
@@ -267,6 +297,13 @@ class MainWindow(QMainWindow, form_class):
             self.textEdit.append("------- END -------")
             self.button3.setText("매매시작")
 
+    def clickBtn4(self):
+
+        tickerKey = self.tickerKey.text()
+        change_ticker(tickerKey)
+        self.ticker = change_ticker(tickerKey)
+        QMessageBox.information(self, "코인정보", "코인이 {}로 바뀌었습니다.".format(self.ticker))
+
     def receiveTradingSignal(self, time, type, amount):
         self.textEdit.append(f"[{time}] {type} : {amount}")
 
@@ -278,7 +315,29 @@ class MainWindow(QMainWindow, form_class):
         self.widget_3.closeEvent(event)
     # ------------------------------------------
 
+
+#현재 token 12시간 유효
+#------json 파일 경로 넣어줄 것---------
+file = '~\\Projects\\git\\autotrading\\kakao_code.json'
+
+
 if __name__ == "__main__":
+    if os.path.exists(file):
+        print("json exist")
+    else:
+        url = "https://kauth.kakao.com/oauth/token"
+
+        data = {
+            "grant_type": "authorization_code",
+            "client_id": "9fe6eedbb07dcabc2528454a986e5a8e",
+            "redirect_url": "https://localhost:3000",
+            "code": "U_oepoFraPHqVmXNFAiNKRhIU_ko2XAt15j-D_QjF2fd0q_PRcnYPjJh-S1ij1dWscZOOgo9dVsAAAF9TKAHBQ"
+        }
+        response = requests.post(url, data=data)
+        tokens = response.json()
+
+        with open("kakao_code.json", "w") as fp:
+            json.dump(tokens, fp)
     app = QApplication(sys.argv)
     mw = MainWindow()
     mw.show()
